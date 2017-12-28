480ada5b49b00457b38e33c47dae506d331cae98
Armin Tamzarian <tamzarian1989@gmail.com>
2017-12-28 14:42:27 -0600

    fix: private scoping for Credentials constructor

--- src/aenigma.model.ts
+++ src/aenigma.model.ts
@@ -4,6 +4,9 @@ import { Observable } from 'rxjs/Observable';
 
 import { AenigmaUtil } from './aenigma.util';
 
+/**
+ * Class to generate and manage encryption credentials for the aenigma library.
+ */
 export class Credentials {
   public static readonly ALGORITHM: string = 'AES-GCM';
   public static readonly BLOCK_SIZE: number = 16;
@@ -11,6 +14,13 @@ export class Credentials {
 
   private _key: JsonWebKey;
 
+  /**
+   * Generate a new encryption credentials and initialze a {@link Credentials} object to manage them
+   *
+   * @param keyLength optional length in bytes for the encryption key (default: 256)
+   *
+   * @returns `Observable` containing the newly generated `Credentials` object
+   */
   public static generate(keyLength = 256): Observable<Credentials> {
     function generateKey(observer: Observer<Credentials>): Promise<void> {
       return (<Promise<CryptoKey>>window.crypto.subtle.generateKey(
@@ -35,6 +45,13 @@ export class Credentials {
     return Observable.create(generateKey);
   }
 
+  /**
+   * Import a previously generated {@link https://tools.ietf.org/html/rfc7517|JsonWebKey} credentials and initialze a {@link Credentials} object to manage them
+   *
+   * @param jwk a previously generated representation of a `JsonWebKey` object
+   *
+   * @returns `Observable` containing the newly generated `Credentials` object
+   */
   public static import(jwk: JsonWebKey): Observable<Credentials> {
     function importKey(observer: Observer<Credentials>): Promise<void> {
       return (<Promise<
@@ -62,7 +79,14 @@ export class Credentials {
     return Observable.create(importKey);
   }
 
-  constructor(key: JsonWebKey) {
+  /**
+   * Protected tandard constructor for the `Credentials` class
+   *
+   * __Note:__ This constructor should not be instantiated directly and should instead be delegated through the `Credentials.import` interface
+   *
+   * @param jwk a `JsonWebKey` object exported through the Web Cryptography `exportKey` functionality
+   */
+  private constructor(key: JsonWebKey) {
     this._key = key;
   }
 
@@ -71,11 +95,28 @@ export class Credentials {
   }
 }
 
+/**
+ * Class representing the JSON properties of an {@Link EncryptedValue} object
+ */
 export class EncryptedValueJson {
+  /**
+   * @param data  base64 representation of the encrypted data
+   * @param iv  base64 representation of the encrypted data's initialization vector
+   */
   constructor(public data: string, public iv: string) {}
 }
 
+/**
+ * Class to manage and store the values of encrypted data
+ */
 export class EncryptedValue {
+  /**
+   * Parse the JSON representaiton of an {@link EncryptedValue} object
+   *
+   * @param json  JSON representation of an `EncryptedValue`
+   *
+   * @returns `EncryptedValue` object for the provided JSON data
+   */
   public static parse(json: string): EncryptedValue {
     const encryptedValueJson: EncryptedValueJson = <EncryptedValueJson>JSON.parse(
       json
@@ -87,8 +128,21 @@ export class EncryptedValue {
     );
   }
 
+  /**
+   * Standard constructor for the {@link EncryptedValue} class
+   *
+   * __Note:__ Under normal usage this constructor should not be instantiated directly and should instead be delegated through the `EncryptedValue.parse` interface
+   *
+   * @param data  encrypted data buffer
+   * @param iv  encrypted data initialization vector array
+   */
   constructor(public data: ArrayBuffer, public iv: Uint8Array) {}
 
+  /**
+   * Generates a JSON string representation for this {@link EncryptedValue} object
+   *
+   * @return  JSON string representation for the `EncryptedValue` object
+   */
   public stringify(): string {
     return JSON.stringify(
       new EncryptedValueJson(
